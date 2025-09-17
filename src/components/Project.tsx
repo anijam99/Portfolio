f38737dd3ed6cfd12f053a56988ac8770c79a02b
jammie <ajammie99@gmail.com>
2025-09-17 22:05:10 +0800

    added additional project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import medisort from '../assets/images/medisort.png';
+import keylogs from '../assets/images/keylogs.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -11,6 +12,11 @@ function Project() {
                 <a href="https://github.com/anijam99/medisort" target="_blank" rel="noreferrer"><img src={medisort} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://github.com/anijam99/medisort" target="_blank" rel="noreferrer"><h2>Medisort</h2></a>
                 <p>A simple Python GUI tool for rapid manual sorting/tagging of large collections of images or videos into custom folders.</p>
+            </div>
+            <div className="project">
+                <a href="https://github.com/anijam99/keylogs" target="_blank" rel="noreferrer"><img src={keylogs} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/anijam99/keylogs" target="_blank" rel="noreferrer"><h2>Keylogs</h2></a>
+                <p>A simple keylogger project written in C.</p>
             </div>
         </div>
     </div>
